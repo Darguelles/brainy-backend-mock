26a17a2092fb4cc975b8f8835b7745128423146f
Diego <darguelles.rojas91@gmail.com>
2016-10-19 17:43:08 -0500

    averageLevel replaced to value

--- profile/endorse_service.rb
+++ profile/endorse_service.rb
@@ -6,10 +6,10 @@ require "sinatra/reloader"
 
 #give support
 post '/profileApi/profile/:email/skill/:idSkill/endorsements' do
-  {:averageLevel => Faker::Number.between(1, 10)}.to_json
+  {:value => Faker::Number.between(1, 10)}.to_json
 end
 
 #quit support
 delete '/profileApi/profile/:email/skill/:idSkill/endorsements' do
-  {:averageLevel => Faker::Number.between(1, 10)}.to_json
+  {:value => Faker::Number.between(1, 10)}.to_json
 end
