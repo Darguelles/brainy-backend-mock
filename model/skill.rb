74659ce27a74b238344c32f9b04e11cc12ff5301
Diego <darguelles.rojas91@gmail.com>
2016-10-21 10:56:24 -0500

    averageLevel added to model

--- model/skill.rb
+++ model/skill.rb
@@ -4,12 +4,14 @@ require 'sinatra/reloader'
 
 class Skill
 
-  attr_accessor :id, :name
+  attr_accessor :id, :name, :myLevel, :averageLevel
 
   def self.mock_skill
     skill = Skill.new
     skill.id =  Faker::Number.between(1, 10)
     skill.name = Faker::Hacker.adjective
+    skill.myLevel = Faker::Number.between(1, 10)
+    skill.averageLevel = Faker::Number.between(1, 10)
     skill
   end
 
--- routes/endorse_routes.rb
+++ routes/endorse_routes.rb
@@ -5,10 +5,14 @@ require 'sinatra/reloader'
 
 #give support
 post '/profileApi/profile/:email/skill/:idSkill/endorsements' do
-  {:value => Faker::Number.between(1, 10)}.to_json
+  {
+      :averageLevel => Faker::Number.between(1, 10)
+  }.to_json
 end
 
 #quit support
 delete '/profileApi/profile/:email/skill/:idSkill/endorsements' do
-  {:value => Faker::Number.between(1, 10)}.to_json
+  {
+      :averageLevel => Faker::Number.between(1, 10)
+  }.to_json
 end
